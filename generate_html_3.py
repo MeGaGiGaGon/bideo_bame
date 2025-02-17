79161052846e435fa9444e09467065335233dba1
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-02-17 08:57:27 -0800

    notan start

--- generate_html_3.py
+++ generate_html_3.py
@@ -16,13 +16,30 @@ wasm_base64 = base64.b64encode(open(release / "wasm_soup" / f"{containing_folder
 js = open(release / "wasm_soup" / f"{containing_folder.name}.js").read().replace("\\", "\\\\").replace("`", "\\`").replace("$", "\\$")
 snippets = set(re.findall(rf"'./(snippets/{containing_folder.name}-([^/]+)/inline\d+.js)'", js))
 
-html = f"""<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>TITLE</title>
-</head>
-<body style="margin:0;padding:0;background-color:black">
-    <canvas width="150" height="150", id="canvas"></canvas>
+html = f"""
+<html>
+    <head>
+        <title>Notan App</title>
+        <meta content="text/html;charset=utf-8" http-equiv="Content-Type"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport"
+              content="minimal-ui, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <style>
+            html, body {{
+                width: 100%;
+                height: 100%;
+                padding: 0;
+                margin: 0;
+                background-color: #252526;
+            }}
+
+            * {{
+                outline: none;
+            }}
+        </style>
+    </head>
+    <body>
     <script type="module" data-info="https://stackoverflow.com/a/43834063">
     const wasm_blob_url = URL.createObjectURL(new Blob([Uint8Array.from(atob("{wasm_base64}"), c => c.charCodeAt(0))], {{type: "application/wasm"}}));
     console.log("WASM blob created");
@@ -55,6 +72,8 @@ document.body.appendChild(script_{name});
 </body>
 
 </html>"""
+
+print(f"HTML size: {re.sub(r"(?<=\d)(?=(\d\d\d)+$)", ",", str(len(html.encode("utf-8"))//1024))} KB")
 
 with open(release / "index.html", "w") as f:
     f.write(html)
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use wasm_bindgen::{prelude::{wasm_bindgen}};
+use wasm_bindgen::prelude::wasm_bindgen;
 
 macro_rules! println {
     ( $( $t:tt )* ) => {
@@ -6,47 +6,257 @@ macro_rules! println {
     }
 }
 
-// macro_rules! function {
-//     ($name:ident, $( $t:tt )* ) => {
-//         #[allow(non_upper_case_globals)]
-//         static mut $name: std::cell::LazyCell<Function> = std::cell::LazyCell::new(|| {Closure::wrap(Box::new( $( $t )* ) as Box<dyn Fn()>).as_ref().unchecked_ref::<Function>().clone()});
-//     };
-// }
-
-// fn test_callback() {
-
-// }
-
-// function!(test_callback, || {});
-// static mut test_callback: std::cell::LazyCell<Function> = std::cell::LazyCell::new(|| {Closure::wrap(Box::new(|| {}) as Box<dyn Fn()>).as_ref().unchecked_ref::<Function>().clone()});
-
-// macro_rules! callback {
-//     ($method:expr, $($args:tt)*, @$($callback:tt)*) => {
-
-//     }
-// }
-
-#[wasm_bindgen(inline_js=r#"
-export function get_canvas_context() {
-    return null;
-    const canvas = document.querySelector("body > canvas");
-    if (canvas.getContext) {
-        return canvas.getContext("2d");
-    } else {
-        return null;
-    }
-}
-"#)]
-extern "C" {
-    fn get_canvas_context() -> Option<web_sys::CanvasRenderingContext2d>;
-}
-
 #[wasm_bindgen(start)]
 fn start() {
     console_error_panic_hook::set_once();
-    // let context = get_canvas_context();
-    println!("{:?}", get_canvas_context());
-    println!("{:?}", get_canvas_context());
-    println!("{:?}", get_canvas_context());
-    // get_canvas_context().unwrap().begin_path();
+    main().unwrap();
+}
+
+use notan::draw::*;
+use notan::prelude::*;
+use notan::random::rand::prelude::*;
+
+const COLS: usize = 7;
+const NUMBERS: usize = COLS * COLS;
+const TILE_SIZE: f32 = 200.0;
+const BOARD_SIZE: f32 = COLS as f32 * TILE_SIZE;
+
+const FILL_COLOR: Color = Color::from_rgb(0.9, 0.9, 0.9);
+const OUTLINE_COLOR: Color = Color::from_rgb(0.0, 0.8, 0.7);
+
+#[notan_main]
+fn main() -> Result<(), String> {
+    let win = WindowConfig::default()
+        // .set_size(BOARD_SIZE as _, BOARD_SIZE as _)
+        .set_multisampling(8)
+        ;
+
+    notan::init_with(State::new)
+        .add_config(win)
+        .add_config(DrawConfig)
+        .update(update)
+        .draw(draw)
+        .build()
+}
+
+fn update(app: &mut App, state: &mut State) {
+    if app.mouse.was_pressed(MouseButton::Left) {
+        if let Some((x, y)) = mouse_pos_to_point(app.mouse.x, app.mouse.y) {
+            if let Some(dir) = state.board.can_move_to(x, y) {
+                let (tx, ty) = match dir {
+                    Direction::Left => (x - 1, y),
+                    Direction::Right => (x + 1, y),
+                    Direction::Up => (x, y - 1),
+                    Direction::Down => (x, y + 1),
+                };
+
+                state.board.move_tile((x, y), (tx, ty));
+                return;
+            }
+        }
+    }
+
+    if state.board.is_solved() && app.mouse.was_pressed(MouseButton::Left) {
+        state.reset();
+    }
+
+    let size = app.window().container_size();
+    app.window().set_size(size.0.try_into().unwrap(), size.1.try_into().unwrap());
+}
+
+fn mouse_pos_to_point(x: f32, y: f32) -> Option<(usize, usize)> {
+    let in_x_bounds = x >= 0.0 && x < TILE_SIZE * COLS as f32;
+    let in_y_bounds = y >= 0.0 && y < TILE_SIZE * COLS as f32;
+    let in_bounds = in_x_bounds && in_y_bounds;
+    if !in_bounds {
+        return None;
+    }
+
+    let xx = (x / TILE_SIZE).floor() as usize;
+    let yy = (y / TILE_SIZE).floor() as usize;
+    Some((xx, yy))
+}
+
+fn draw(gfx: &mut Graphics, state: &mut State) {
+    let mut draw = gfx.create_draw();
+    draw.clear(Color::BLACK);
+
+    for y in 0..COLS {
+        for x in 0..COLS {
+            let value = state.board.value(x, y);
+            draw_tile(&mut draw, &state.font, x, y, value);
+        }
+    }
+
+    if state.board.is_solved() {
+        let (ww, hh) = gfx.size();
+        let (ww, hh) = (ww as f32, hh as f32);
+
+        draw.rect((0.0, 0.0), (ww, hh))
+            .color(Color::BLACK)
+            .alpha(0.7);
+
+        draw.text(&state.font, "Done!")
+            .color(Color::ORANGE)
+            .size(74.0)
+            .position(ww * 0.5, hh * 0.5)
+            .h_align_center()
+            .v_align_bottom();
+
+        draw.text(&state.font, "Tap to reset")
+            .color(Color::GRAY)
+            .size(54.0)
+            .v_align_top()
+            .h_align_center()
+            .position(ww * 0.5, hh * 0.5 + 20.0);
+    }
+
+    gfx.render(&draw);
+}
+
+fn draw_tile(draw: &mut Draw, font: &Font, x: usize, y: usize, value: u8) {
+    if value == 0 {
+        return;
+    }
+
+    let xx = x as f32 * TILE_SIZE;
+    let yy = y as f32 * TILE_SIZE;
+    draw.rect((xx, yy), (TILE_SIZE, TILE_SIZE))
+        .corner_radius(10.0)
+        .fill_color(FILL_COLOR)
+        .fill()
+        .stroke_color(OUTLINE_COLOR)
+        .stroke(5.0);
+
+    draw.text(font, &format!("{value}"))
+        .color(Color::BLACK)
+        .size(34.0)
+        .position(xx + TILE_SIZE * 0.5, yy + TILE_SIZE * 0.5)
+        .h_align_center()
+        .v_align_middle();
+}
+
+#[derive(AppState)]
+struct State {
+    font: Font,
+    board: Board,
+}
+
+impl State {
+    fn new(gfx: &mut Graphics) -> Self {
+        let font = gfx
+            .create_font(include_bytes!("NotCourierSans.ttf"))
+            .unwrap();
+        let board = Board::new();
+
+        Self { font, board }
+    }
+
+    fn reset(&mut self) {
+        self.board = Board::new();
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+struct Board {
+    grid: [u8; NUMBERS],
+}
+
+impl Board {
+    /// Create a new board using random numbers
+    fn new() -> Self {
+        // generate an initial board, and move randomly from it.
+
+        let mut grid = [0; NUMBERS];
+        grid.iter_mut().enumerate().for_each(|(i, n)| {
+            if i == NUMBERS - 1 {
+                return;
+            }
+
+            *n = i as u8 + 1;
+        });
+
+        // move blank cell randomly.
+
+        let mut x = (COLS - 1) as i32;
+        let mut y = (COLS - 1) as i32;
+
+        for _ in 0..1000 {
+            const DIRS: [(i32, i32); 4] = [(0, -1), (-1, 0), (1, 0), (0, 1)];
+            let (dx, dy) = DIRS.choose(&mut thread_rng()).unwrap();
+            let x_nxt = x + dx;
+            let y_nxt = y + dy;
+
+            const RANGE: std::ops::Range<i32> = 0..COLS as i32;
+            if !RANGE.contains(&x_nxt) || !RANGE.contains(&y_nxt) {
+                continue;
+            }
+
+            let index = index_from_point(x as usize, y as usize);
+            let index_nxt = index_from_point(x_nxt as usize, y_nxt as usize);
+            grid.swap(index, index_nxt);
+
+            x = x_nxt;
+            y = y_nxt;
+        }
+
+        Self { grid }
+    }
+
+    fn is_solved(&self) -> bool {
+        for i in 0..NUMBERS - 1 {
+            if self.grid[i] != i as u8 + 1 {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn value(&self, x: usize, y: usize) -> u8 {
+        let index = index_from_point(x, y);
+        self.grid[index]
+    }
+
+    fn is_empty(&self, x: usize, y: usize) -> bool {
+        self.value(x, y) == 0
+    }
+
+    fn move_tile(&mut self, pos1: (usize, usize), pos2: (usize, usize)) {
+        let index1 = index_from_point(pos1.0, pos1.1);
+        let index2 = index_from_point(pos2.0, pos2.1);
+        self.grid.swap(index1, index2);
+    }
+
+    fn can_move_to(&self, x: usize, y: usize) -> Option<Direction> {
+        if x >= 1 && self.is_empty(x - 1, y) {
+            return Some(Direction::Left);
+        }
+
+        if x <= COLS - 2 && self.is_empty(x + 1, y) {
+            return Some(Direction::Right);
+        }
+
+        if y >= 1 && self.is_empty(x, y - 1) {
+            return Some(Direction::Up);
+        }
+
+        if y <= COLS - 2 && self.is_empty(x, y + 1) {
+            return Some(Direction::Down);
+        }
+
+        None
+    }
+}
+
+#[inline]
+fn index_from_point(x: usize, y: usize) -> usize {
+    debug_assert!(x < COLS || y < COLS, "Point index out of bounds.");
+    y * COLS + x
 }
